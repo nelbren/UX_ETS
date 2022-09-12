71ce141842ca81bbfbc8106aedd5e0937aa52f72
Martin Cuellar <nelbren@gmail.com>
2022-09-11 20:11:01 -0600

    swagger_001

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,19 @@ import user from './user'
 
 const routes  = Router()
 
+const path = require('path')
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerSpec = {
+  definition: {
+    openapi: "3.0.0",
+    info: { title: "Express+TypeORM+Swagger API", version: "1.0.0" },
+    servers: [ { url: "http://localhost:3000" } ]
+  },
+  apis: [ `${path.join(__dirname, './*.ts')}`],
+}
+routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(swaggerSpec), { explorer: true }));
+
 routes.use('/user', user)
 
 export default routes
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,6 +3,40 @@ import { UserController } from '../controllers'
 
 const router = Router()
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *        Type: object
+ *        properties:
+ *          firstName: 
+ *            type: string
+ *            description: Nombre
+ *          lastname:
+ *            type: string
+ *            description: Apellido
+ *          age:
+ *            type: number
+ *            description: Edad
+ */
+
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *      summary: Obtiene todos los usuarios
+ *      tag: [User]
+ *      responses:
+ *         200:
+ *           description: Lista de usuarios
+ *           content:
+ *             application/json:
+ *               schema:
+ *                  type: array
+ *                  items:
+ *                    $ref: '#/components/schemas/User'
+ */
 router.get('', UserController.GetAll)
 
 export default router
